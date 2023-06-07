b7bee0d4abd0e9d3e6129f6385a8bd3a25ac7ef1
Evan Prodromou <evan@openearth.org>
2023-06-07 11:12:24 -0400

    feat(emissions): calculate emissions values
    Signed-off-by: Evan Prodromou <evan@openearth.org>

--- __tests__/util.test.ts
+++ __tests__/util.test.ts
@@ -1,15 +1,24 @@
-import { actorNextTarget } from '../src/lib/util';
+import { actorNextTarget, actorEmissions } from '../src/lib/util';
 import fs from 'fs';
 import path from 'path';
 
 describe('Util', () => {
     const CA = JSON.parse(fs.readFileSync(path.join(__dirname, 'CA.json'), 'utf8')).data;
-    console.dir(CA.actor_id)
     describe('actorNextTarget', () => {
         test('returns the next target for the given actor', () => {
             const target = actorNextTarget(CA);
-            expect(target.target_year).toBe(2030);
-            expect(target.target_value).toBe("40");
+            expect(target?.target_year).toBe(2030);
+            expect(target?.target_value).toBe("40");
         });
-    })
+    });
+    describe('actorEmissions', () => {
+        test('returns the actor\'s emissions for a past year', () => {
+            const emissions2005 = actorEmissions(CA, 2005);
+            expect(Math.round(emissions2005/1000000)).toBeCloseTo(741);
+        });
+        test('returns the actor\'s emissions for a future year', () => {
+            const emissions2030 = actorEmissions(CA, 2030);
+            expect(Math.round(emissions2030/1000000)).toBeCloseTo(445);
+        });
+    });
 });
--- src/lib/util.ts
+++ src/lib/util.ts
@@ -1,11 +1,23 @@
 import { Target } from "./models"
 
+const dsq = {
+    "ECCC:GHG_inventory:2022-04-13": 0.9,
+    "EPA:state_GHG_inventory:2022-08-31": 0.8,
+    "PRIMAP:10.5281/zenodo.7179775:v2.4": 0.8,
+    "UNFCCC:GHG_ANNEX1:2019-11-08": 0.9,
+    "WRI:climate_watch_historical_ghg:2022": 0.7
+};
+
 // returns the next target for the given actor
 // returns null if there is no next target
 
-export function actorNextTarget(actorDetails: any): any {
+export function actorNextTarget(actorDetails: any): Target | null {
+    return actorTargetAfter(actorDetails, (new Date()).getFullYear());
+}
+
+function actorTargetAfter(actorDetails: any, year: number): Target | null {
     const filtered = actorDetails.targets
-        .filter((target: any) => target.target_type == "Absolute emission reduction");
+        .filter((target: any) => target.target_type == "Absolute emission reduction" && target.target_year >= year);
     const sorted = filtered
         .sort((a: any, b: any) => (a.target_year > b.target_year) ? 1 :
             (a.target_year < b.target_year) ? -1 :
@@ -22,7 +34,39 @@ export function actorNextTarget(actorDetails: any): any {
 // calculated from the actor's targets
 
 export function actorEmissions(actorDetails: any, year: number): number {
-    return (actorDetails.type == 'country') ? 100000000: 1000000;
+    if (year < (new Date()).getFullYear()) {
+        return actorEmissionsPast(actorDetails, year);
+    } else {
+        return actorEmissionsFuture(actorDetails, year);
+    }
+}
+
+function actorEmissionsPast(actorDetails: any, year: number): number {
+    let values = []
+    for (let datasource_id in actorDetails.emissions) {
+        const datasource = actorDetails.emissions[datasource_id];
+        const emissions = datasource.data.find((emission: any) => emission.year == year);
+        if (emissions) {
+            const quality = (datasource_id in dsq) ? dsq[datasource_id] : 0.0;
+            values.push({total_emissions: emissions.total_emissions, quality: quality});
+        }
+    }
+    let sorted = values.sort((a: any, b: any) => b.quality - a.quality);
+    return (sorted) ? sorted[0].total_emissions : null;
+}
+
+function actorEmissionsFuture(actorDetails: any, year: number): number {
+    const target = actorTargetAfter(actorDetails, year);
+    if (target) {
+        const emissions = actorEmissionsPast(actorDetails, target.baseline_year);
+        const total_reduction = (target.target_unit == "percent") ?
+            ((parseFloat(target.target_value) / 100) * emissions) :
+            parseFloat(target.target_value);
+        const reduction_for_year = ((year - target.baseline_year) / (target.target_year - target.baseline_year)) * total_reduction;
+        return emissions - reduction_for_year;
+    } else {
+        return Infinity;
+    }
 }
 
 // returns the actor's emissions reductions between the given
