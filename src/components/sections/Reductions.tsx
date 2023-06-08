8fdce1157cb2b1db5bb3559405d2edef05ba7dd4
Milan Gruner <milangruner@gmail.com>
2023-06-08 22:24:30 +0200

    fix: add padding to reductions

--- src/components/sections/Reductions.tsx
+++ src/components/sections/Reductions.tsx
@@ -4,7 +4,7 @@ import Container from '@mui/material/Container';
 
 const Reductions = () => {
     return(
-        <div>
+        <div className="py-8">
             <Container maxWidth="xl" className='flex items-center h-full'>
                 <p className='text-3xl font-bold'>Reductions for the next national target year (2030)</p>
                 {/* Graph can go here */}
