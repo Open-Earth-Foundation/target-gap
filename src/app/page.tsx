0884dee682406b7a3b8e37a0dc0c83f1f40a1654
cephaschapa <cephaschapa@gmail.com>
2023-09-05 14:48:04 +0200

    feat(ON-753): implemented tab selection form mobile, style changes to tabs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,17 @@ import Hero from "@/components/header/Hero";
 import { TabPanel } from "@/components/tabs/TabPanel";
 import { TargetGapTab } from "@/components/tabs/TargetGapTab";
 import { ArrowForward } from "@mui/icons-material";
-import { Box, Button, Tab, Tabs } from "@mui/material";
+import {
+  Box,
+  Button,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Tab,
+  Tabs,
+} from "@mui/material";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
 
 function a11yProps(index: number) {
   return {
@@ -20,11 +30,17 @@ export default function Home() {
   const handleTabChange = (event: SyntheticEvent, newValue: number) => {
     setSelectedTab(newValue);
   };
+  const handleTabChangeMobile = (event: any) => {
+    setSelectedTab(event.target.value);
+  };
 
   const contentRef = useRef<null | HTMLDivElement>(null);
   const scrollToContent = () => {
     contentRef.current?.scrollIntoView();
   };
+
+  // listen to media queries that match css
+  const matches = useMediaQuery("(min-width: 600px)");
 
   return (
     <div className="bg-[#FAFAFA]">
@@ -34,22 +50,58 @@ export default function Home() {
           className="rounded-full px-6 py-4 text-white"
           color="secondary"
           endIcon={<ArrowForward />}
-          onClick={scrollToContent}
-        >
+          onClick={scrollToContent}>
           Start Exploring
         </Button>
       </Hero>
       <div className="p-16" ref={contentRef}>
-        <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-          <Tabs
-            value={selectedTab}
-            onChange={handleTabChange}
-            aria-label="basic tabs example"
-          >
-            <Tab label="Target Gap Visualizer" {...a11yProps(0)} />
-            <Tab label="Reduction Progress" {...a11yProps(1)} />
-            <Tab label="Actors with Targets" {...a11yProps(2)} />
-          </Tabs>
+        <Box
+          sx={{
+            borderBottom: { xs: "none", md: 1 },
+            borderColor: { xs: "none", md: "divider" },
+          }}>
+          {matches ? (
+            <Tabs
+              value={selectedTab}
+              onChange={handleTabChange}
+              aria-label="basic tabs example"
+              textColor="primary"
+              indicatorColor="primary">
+              <Tab label="Target Gap Visualizer" {...a11yProps(0)} />
+              <Tab label="Reduction Progress" {...a11yProps(1)} />
+              <Tab label="Actors with Targets" {...a11yProps(2)} />
+            </Tabs>
+          ) : (
+            <Box>
+              <FormControl fullWidth>
+                <p className="text-center pb-2 text-[12px] font-semibold text-[#7A7B9A] tracking-[0.5px]">
+                  Select a visualization
+                </p>
+                <Box className="flex w-full justify-center">
+                  <Select
+                    sx={{
+                      border: "1px solid #D7D8FA",
+                      borderRadius: "8px",
+                      width: "209px",
+                      height: "44px",
+                      fontWeight: 500,
+                      lineHeight: "20px",
+                      letterSpacing: "0.5px",
+                      ":focus": {
+                        borderColor: "blue",
+                      },
+                    }}
+                    id="demo-simple-select"
+                    value={selectedTab}
+                    onChange={handleTabChangeMobile}>
+                    <MenuItem value={0}>Target Gap Visualizer</MenuItem>
+                    <MenuItem value={1}>Reduction Progress</MenuItem>
+                    <MenuItem value={2}>Actors with Targets</MenuItem>
+                  </Select>
+                </Box>
+              </FormControl>
+            </Box>
+          )}
         </Box>
         <TabPanel value={selectedTab} index={0}>
           <TargetGapTab />
