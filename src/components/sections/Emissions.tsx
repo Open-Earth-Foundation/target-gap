70f3471aab14c11db72c88262332bc4839195998
Milan Gruner <milangruner@gmail.com>
2023-09-06 18:55:23 +0200

    fix: headings not wrapping for Emissions and Reductions diagrams
    Signed-off-by: Milan Gruner <milangruner@gmail.com>

--- src/components/sections/Emissions.tsx
+++ src/components/sections/Emissions.tsx
@@ -137,7 +137,7 @@ const Emissions: FunctionComponent<EmissionsProps> = ({ actor, parts }) => {
   return (
     <Card sx={{ minWidth: 400, minHeight: 400 }} className="overflow-visible w-full lg:inline-block lg:w-1/2">
       <CardContent>
-        <p className="text-lg"><span className="font-bold">Emissions</span> for the next national target year ({targetYear})</p>
+        <p className="text-lg whitespace-break-spaces"><span className="font-bold">Emissions</span> for the next national target year ({targetYear})</p>
         <p className="text-xs text-gray-500 pb-2">Last updated in 2019</p>
         {hasMissingData ? (
           <p className="text-center align-center w-full my-32">Insufficient data for this country</p>
