70f8a1a0bd85ba4ba723bce90a00415117634f24
Milan Gruner <milangruner@gmail.com>
2023-09-08 16:26:08 +0200

    feat: update metadata for social sharing
    Signed-off-by: Milan Gruner <milangruner@gmail.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,25 @@
-import Navbar from '@/components/navbar/Navbar';
-import './globals.css';
-import { Poppins } from 'next/font/google';
-import Footer from '@/components/footer/PageFooter';
-import ThemeRegistry from '@/components/ThemeRegistry/ThemeRegistry';
+import Navbar from "@/components/navbar/Navbar";
+import "./globals.css";
+import { Poppins } from "next/font/google";
+import Footer from "@/components/footer/PageFooter";
+import ThemeRegistry from "@/components/ThemeRegistry/ThemeRegistry";
 
 const poppins = Poppins({
-  subsets: ['latin'],
+  subsets: ["latin"],
   weight: ["300", "400", "500", "600"],
 });
 
 export const metadata = {
-  title: 'Target Gap Visualiser',
-  description: 'Open Earth Foundation Target Gap Visualiser',
-}
+  title: "DIGS Data Explorer",
+  description:
+    "Using data available at OpenClimate, we&apos;ve conducted a Digitally Enabled Independent Global Stocktake exercise to spotlight the alignment and gaps between climate targets and actions of nations, regions, and cities.",
+};
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en" className="scroll-smooth">
       <body className={poppins.className}>
@@ -25,6 +30,5 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         </ThemeRegistry>
       </body>
     </html>
-  )
+  );
 }
-
